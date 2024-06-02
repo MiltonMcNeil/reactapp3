b98e637e6f86081a921b4a431f2e210242929d9d
JBQ1 JBQ1 <quizkid91102@gmail.com>
2024-06-02 00:49:09 -0400

    first commit

---
+++ src/api/server.ts
@@ -0,0 +1,55 @@
+let token = '89fdf4d6899e7230bc7539f1c202fb0d1e2d90d7e28b8ad8'
+
+export const server_calls = {
+    get: async () => {
+        const response = await fetch(`https://in-class-code-along.onrender.com/`,{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': `Bearer ${token}`
+            }
+        });
+
+        if (!response.ok){
+            throw new Error('Failed to fetch data from server')
+        }
+
+        return await response.json()
+    },
+
+    create: async(data: any = {}) => {
+        const response = await fetch(`https://in-class-code-along.onrender.com/`,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': `Bearer ${token}`
+            },
+            body: JSON.stringify(data)
+        });
+
+        if(!response.ok){
+            throw new Error('Failed to Create new data on server')
+        }
+
+        return await response.json()
+    },
+    update: async (id:string, data:any = {}) => {
+        const response = await fetch(`https://in-class-code-along.onrender.com/${id}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': `Bearer ${token}`
+            },
+            body: JSON.stringify(data)
+        });
+    },
+    delete: async(id:string) => {
+        const response = await fetch(`https://in-class-code-along.onrender.com/${id}`,{
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': `Bearer ${token}`
+            }
+        })
+    }
+}
---
+++ src/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBO0Nux02hxeUMZ_LKjq7KYha98x45OOjw",
+  authDomain: "whisky-594ca.firebaseapp.com",
+  projectId: "whisky-594ca",
+  storageBucket: "whisky-594ca.appspot.com",
+  messagingSenderId: "989315235770",
+  appId: "1:989315235770:web:a954786f31c7b3c6732783"
+};
